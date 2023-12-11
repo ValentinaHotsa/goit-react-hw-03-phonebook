a2ed65a609f1f84f9ebe17dd011d240dd596a762
ValentinaHotsa <134721626+ValentinaHotsa@users.noreply.github.com>
2023-12-11 10:37:59 +0100

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -55,7 +55,6 @@ export class App extends Component {
     return (
       <div
         style={{
-          // height: '100vh',
           display: 'flex',
           flexDirection: 'column',
           justifyContent: 'center',
